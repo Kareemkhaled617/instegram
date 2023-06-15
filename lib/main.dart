d5943047897d6f5f09072d900108f34a36a62693
kamelshaltout <kamelshaltout@gmail.com>
2023-06-15 23:08:40 +0300

    1121

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,9 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   // initialise app based on platform- web or mobile
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyCZ-xrXqD5D19Snauto-Fx_nLD7PLrBXGM",
-          appId: "1:585119731880:web:eca6e4b3c42a755cee329d",
-          messagingSenderId: "585119731880",
-          projectId: "instagram-clone-4cea4",
-          storageBucket: 'instagram-clone-4cea4.appspot.com'),
-    );
-  } else {
     await Firebase.initializeApp();
-  }
+
   runApp(const MyApp());
 }
 
